6867b66f0df04f680ed5a104e422dd53e702fa60
Shivam-Yadav <shivam.yadav@st.niituniversity.in>
2017-01-27 01:28:40 +0530

    First commit

---
+++ website_blocker.py
@@ -0,0 +1,9 @@
+__author__ = 'Shivam'
+import time
+hosts_path=r"C:\Windows\System32\drivers\etc\hosts" #telling python that its a row to avoid '\n' type case
+redirect="127.0.0.1"
+website_list=["www.facebook.com","cyro.se","kissanime.io","facebook.com"]
+
+while(False):
+    print(1)
+    time.sleep(10)
